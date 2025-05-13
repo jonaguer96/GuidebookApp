fab2ff1fd74765ed12b017757c61401433d47cfc
Jonathan Guerrero <jona.guerrero@live.com>
2025-05-12 22:05:05 -0500

    Initial Commit

---
+++ GuidebookApp/GuidebookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookAppApp.swift
+//  GuidebookApp
+//
+//  Created by Jonathan Guerrero on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuidebookAppTests/GuidebookAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookAppTests.swift
+//  GuidebookAppTests
+//
+//  Created by Jonathan Guerrero on 5/12/25.
+//
+
+import Testing
+@testable import GuidebookApp
+
+struct GuidebookAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
